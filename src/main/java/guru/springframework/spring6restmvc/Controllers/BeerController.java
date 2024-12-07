c0df9be24213bfb7864109fb42cbbf8a8a9eb7e8
Bren <bpgit503@gmail.com>
2024-12-07 15:30:44 +0100

    enabled logging and added logs

--- src/main/java/guru/springframework/spring6restmvc/Controllers/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/Controllers/BeerController.java
@@ -1,12 +1,26 @@
 package guru.springframework.spring6restmvc.Controllers;
 
+import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.service.BeerService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 
+import java.util.UUID;
+
+@Slf4j
 @Controller
 @AllArgsConstructor
 public class BeerController {
+
+
     private final BeerService beerService;
+
+    public Beer getBeerById(UUID id){
+
+        log.debug("Get Beer by id - in controller");
+
+        return beerService.getBeerByID(id);
+    }
 
 }
