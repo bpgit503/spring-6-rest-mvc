95a961f7df1bebe07d473f559f7c81c4435ced38
Bren <bpgit503@gmail.com>
2024-12-09 12:24:48 +0100

    created beer list and added it to the controller

--- src/main/java/guru/springframework/spring6restmvc/Controllers/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/Controllers/BeerController.java
@@ -4,19 +4,26 @@ import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.service.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
-@Controller
+@RestController
 @AllArgsConstructor
 public class BeerController {
 
 
     private final BeerService beerService;
 
-    public Beer getBeerById(UUID id){
+    @RequestMapping("/api/v1/beer")
+    public List<Beer> listBeer() {
+        return beerService.listBeer();
+    }
+
+    public Beer getBeerById(UUID id) {
 
         log.debug("Get Beer by id - in controller");
 
