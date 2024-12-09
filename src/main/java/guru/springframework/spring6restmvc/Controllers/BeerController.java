e73579445b79e61b8d8453c62868a06675e3b6f0
Bren <bpgit503@gmail.com>
2024-12-09 12:58:00 +0100

    added endpoints to the controller and tested get beer by id

--- src/main/java/guru/springframework/spring6restmvc/Controllers/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/Controllers/BeerController.java
@@ -4,6 +4,8 @@ import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.service.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,21 +15,23 @@ import java.util.UUID;
 @Slf4j
 @RestController
 @AllArgsConstructor
+@RequestMapping("/api/v1/beer")
 public class BeerController {
 
 
     private final BeerService beerService;
 
-    @RequestMapping("/api/v1/beer")
+    @GetMapping
     public List<Beer> listBeer() {
         return beerService.listBeer();
     }
 
-    public Beer getBeerById(UUID id) {
+    @GetMapping("/{beerId}")
+    public Beer getBeerById(@PathVariable UUID beerId) {
 
         log.debug("Get Beer by id - in controller");
 
-        return beerService.getBeerByID(id);
+        return beerService.getBeerByID(beerId);
     }
 
 }
